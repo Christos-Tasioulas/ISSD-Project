e46b020e6cc554f6399efd2436a3d6afff48f263
User <christasioulas2@gmail.com>
2022-11-04 21:19:13 +0200

    Updated Hopscotch test and merged partitioned hash join test

--- source/Test/Test.cpp
+++ source/Test/Test.cpp
@@ -1,5 +1,9 @@
 #include "acutest.h"
 #include "PartitionedHashJoin.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 static unsigned int hash_int(void *i)
 {
@@ -17,28 +21,42 @@ void hopScotchTest()
 
     TEST_ASSERT(*((int *) ht->getTable()[5].getKey()) == 5);
 
+    int newKey = 5;
+
+    ht->insert(&newKey, &newKey, hash_int);
+
+    // This test indicates that a key that has been entered before moves to the next index
+    TEST_ASSERT(*((int *) ht->getTable()[6].getKey()) == 5);
+
+    int newnewKey = 6;
+
+    ht->insert(&newnewKey, &newnewKey, hash_int);
+
+    // This test indicates that a key that its original index is occupied moves to the next index
+    TEST_ASSERT(*((int *) ht->getTable()[7].getKey()) == 6);
+
+    int key_4 = 6;
+
+    ht->insert(&key_4, &key_4, hash_int);
+
+    int key_5 = 5;
+
+    // A rehash will happen 
+    ht->insert(&key_5, &key_5, hash_int);
+
+    // The bitmap of each index is larger now
+    TEST_ASSERT(*((int *) ht->getTable()[9].getKey()) == 5);
+
     delete ht;
 }
 
-void partitionTest()
+void partitionedHashJoinTest()
 {
 
-}
-
-void buildingTest()
-{
-
-}
-
-void probingTest()
-{
-    
 }
 
 TEST_LIST = {
     { "Hopscotch",  hopScotchTest},
-    { "Partition", partitionTest},
-    { "Building", buildingTest},
-    { "Probing", probingTest},
+    { "PartitionedHashJoin", partitionedHashJoinTest},
     { NULL, NULL }
 };
